94d8c0639a150a25d11847f7b8d42bfbd14f1000
Aleksandr <48583343+ALNIuser@users.noreply.github.com>
2019-12-03 15:14:54 +0300

    Update models.py space

--- myfirst/apps/articles/models.py
+++ myfirst/apps/articles/models.py
@@ -4,6 +4,7 @@ class Article(models.Model):
     article_title = models.CharField('название статьи', max_length= 200)
     article_text = models.TextField('текст статьи')
     pub_date = models.DateTimeField('дата публикации')
+
 class Coment(models.Model):
     article = models.ForeignKey(Article, on_delete = models.CASCADE)
     author_name = models.CharField('имя автора', max_length= 50)
