e465aa5e876454a0759796b8a352517efff1abfc
Aleksandr <48583343+ALNIuser@users.noreply.github.com>
2019-12-06 21:03:14 +0300

    Update space

--- myfirst/apps/articles/models.py
+++ myfirst/apps/articles/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Article(models.Model):
     article_title = models.CharField('название статьи', max_length= 200)
     article_text = models.TextField('текст статьи')
-    pub_date = models.DateTimeField('дата публикации')
+    pub_date = models.DateTimeField('дата публикации ')
 
 class Coment(models.Model):
     article = models.ForeignKey(Article, on_delete = models.CASCADE)
